9b6a7f4bfcf25fa0a6ab6c194d47e2cea69bab01
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-11-19 08:30:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const helmet = require('helmet')
 const compression = require('compression')
 const rateLimit = require('express-rate-limit')
 const { body, check } = require('express-validator')
-const fileHandler = require("fs");
 
 const isProduction = process.env.NODE_ENV === 'production'
 const origin = {
-  origin: isProduction ? 'https://tax-client.now.sh' : '*',
+  origin: isProduction ? 'https://www.example.com' : '*',
 }
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors(origin))
 app.use(compression())
@@ -25,75 +25,71 @@ const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
     max: 5, // 5 requests,
   })
-  
+
   app.use(limiter)
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
+  //show salary
 
-app.use(express.json());
-const json = fileHandler.readFileSync("salaries.json");
-const salaries = JSON.parse(json);
+  const getSalaries = (request, response) => {
+    pool.query('SELECT * FROM salaries', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
 
-//show
-app.get("/salaries", (req, res) => {
-  fileHandler.readFile("salaries.json", (err, data) => {
-    if (err) res.send("File not found");
-    else res.send(data);
-  });
-});
+  //show by ID
 
-//show by ID
-app.get("/salaries/:id", (req, res) => {
-  const data = salaries.find(t => t.id === parseInt(req.params.id));
-  fileHandler.readFile("salaries.json", data, err => {
-    if (err) res.send("File not found");
-    else res.send(data);
-  });
-});
+  const getSalariesById = (request, response) => {
+    const id = parseInt(request.params.id)
 
-//create
-app.post("/salaries", (req, res) => {
-  const item = {
-    id: salaries.length + 1,
-    name: req.body.name,
-    salary: req.body.salary,
-    monthlyTax: req.body.monthlyTax
-  };
-  salaries.push(item);
-  const data = JSON.stringify(salaries, null, 2);
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
 
-  fileHandler.writeFile("salaries.json", data, err => {
-    if (err) throw err;
-    res.send("File created");
-  });
-});
+// POST salaries
 
-//delete
-app.delete("/salaries", (req, res) => {
-  const item = salaries.find(t => t.id === parseInt(req.params.id));
+  const addSalaries = (request, response) => {
+    const { name, salary, monthlyTax } = request.body
 
-  const index = salaries.indexOf(item);
-  salaries.splice(index, 1);
+    pool.query('INSERT INTO salaries (name, salary, monthlyTax) VALUES ($1, $2, $3)', [name, salary, monthlyTax], error => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({ status: 'success', message: 'salaries added.' })
+    })
+  }
 
-  const data = JSON.stringify(salaries, null, 2);
 
-  fileHandler.writeFile("salaries.json", data, err => {
-    if (err) throw err;
-    res.send("File created");
-  });
-});
+  const deleteSalaries = (request,response) => {
+    const id = parseInt(request.params.id)
 
-app.use(function(err, req, res, next) {
-  console.log(err.stack);
-  res.status(500).send("Something broke!");
-});
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User deleted with ID: ${id}`)
+    })
+  }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+
+  app
+    .route('/salaries')
+    // GET endpoint
+    .get(getSalaries)
+    .get(getSalariesById)
+    // POST endpoint
+    .post(addSalaries)
+    //DELETE endpoint
+    .delete(deleteSalaries)
+
+  // Start server
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+  }) 
+
